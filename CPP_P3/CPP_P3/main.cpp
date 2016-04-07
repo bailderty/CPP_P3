565d12596c2e9e3c51ad616fed6faa7c85ac5e8a
bailderty <brettmeyer@Bretts-Mac-mini.local>
2016-04-07 10:24:09 -0700

    Added SortedList.h and Studen.h

---
+++ CPP_P3/CPP_P3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_P3
+//
+//  Created by Brett Meyer on 4/7/16.
+//  Copyright © 2016 Brett Meyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
